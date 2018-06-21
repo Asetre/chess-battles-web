5f98d14106b4b795db3bd148628509d926fb3c0a
unknown <asetre.paul@gmail.com>
2018-06-21 16:59:20 -0700

    auth lock implemented, todo: remove auth0 redirect

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import styled from 'styled-components'
-import Auth from '../auth0.js'
 import Btn from './button'
+import Auth0Lock from 'auth0-lock'
+import axios from 'axios'
+import {serverUrl} from '../config'
+
+const lock = new Auth0Lock(
+  'M1pfmr2L43kkkgA4wo7R1y26EsPhahAd',
+  'paul-asetre.auth0.com'
+)
+
+lock.on("authenticated", function(authResult) {
+  // Use the token in authResult to getUserInfo() and save it to localStorage
+  lock.getUserInfo(authResult.accessToken, function(err, profile) {
+    if (err) {
+      console.log(err)
+      return;
+    }
+
+    if (profile.sub) {
+      axios.get(`${serverUrl}/users/login/${profile.sub}/${profile.nickname}`)
+      .then((res) => {
+        if(res.status !== 200) throw 'Failed to get user profile'
+
+        console.log(res)
+
+        localStorage.setItem('accessToken', authResult.accessToken);
+        localStorage.setItem('profile', JSON.stringify(profile));
+      })
+    }
+
+  });
+});
 
 const StyledNavbar = styled.div`
 display: flex;
@@ -19,15 +49,14 @@ class Navbar extends React.Component {
     super()
     this.handleLogin = this.handleLogin.bind(this)
     this.handleSignup = this.handleSignup.bind(this)
-    this.auth = new Auth()
   }
 
   handleLogin() {
-    this.auth.login()
+    lock.show()
   }
 
   handleSignup() {
-    this.auth.login()
+    lock.show()
   }
 
   render() {

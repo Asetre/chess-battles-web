d07cecd97601325e744a9eedad9190b0ee54be5d
Asetre <asetre.paul@gmail.com>
2018-07-31 02:05:23 -0700

    server fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const serverUrl = 'https://ec2-18-222-141-248.us-east-2.compute.amazonaws.com'
+export const serverUrl = 'https://chess-server-1520337744.us-east-2.elb.amazonaws.com'
 export const gameServerUrl = process.env.gameServerUrl || 'http://localhost:8080'
 
 export const firebaseConfig = {
